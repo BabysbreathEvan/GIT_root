a242527f2949392a58a32959608ff3b69b95b5be
BabysbreathEvan <jiangnanyuxiang@163.com>
2017-12-18 15:47:14 +0800

    Add the u-boot source code

---
+++ u-boot/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# compile the the u-boot 
+#Date: 2017-12-12
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabi-
+ 
+
+#make  vexpress_ca5x2_defconfig
+#make  vexpress_ca5x4_defconfig
+
+#make 
---
+++ u-boot/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,5 @@
+#define U_BOOT_DATE "Nov 20 2017"
+#define U_BOOT_TIME "13:56:04"
+#define U_BOOT_TZ "+0800"
+#define U_BOOT_DMI_DATE "11/20/2017"
+#define U_BOOT_BUILD_DATE 0x20171120
